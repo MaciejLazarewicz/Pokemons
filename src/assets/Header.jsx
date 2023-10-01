d602177c33e5a337535323e76d3cf38a4551d403
Maciej Lazarewicz <maciej.lazarewicz@gmail.com>
2023-10-01 12:08:59 +0200

    test

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -15,7 +15,7 @@ function Header() {
   return (
     <Box
       display="flex"
-      bgColor="#F50057"
+      bgColor="blue"
       justifyContent="space-between"
       width="100%"
       ml={0}
