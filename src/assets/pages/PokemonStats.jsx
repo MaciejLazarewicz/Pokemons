5f163703f04781b4711645b88b43e58f57ac79b0
Maciej Lazarewicz <maciej.lazarewicz@gmail.com>
2023-10-06 14:41:09 +0200

    styling

--- src/assets/pages/PokemonStats.jsx
+++ src/assets/pages/PokemonStats.jsx
@@ -17,7 +17,7 @@ export const PokemonStats = ({
   height,
   weight,
   ability,
-  habitat,
+  gender,
   text
 }) => {
   const backgroundColor = typeColors[type]
@@ -25,7 +25,45 @@ export const PokemonStats = ({
   return (
     <Box display="flex" flexDir="column" width="100%" textTransform="capitalize">
       <Header />
-      <Box display="grid" gridTemplateColumns="repeat(auto-fit,minmax(200px,1fr))">
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(auto-fit,minmax(200px,1fr))"
+        width="70%"
+        bgColor="#f51"
+        justifyItems="center">
+        <Box
+          width="200px"
+          height="200px"
+          display="flex"
+          textTransform="capitalize"
+          flexDir="column"
+          alignItems="center">
+          <Image pl={40} pt={20} width="250px" height="250px" src={image} alt={name} />
+          <Box
+            textTransform="capitalize"
+            border="none"
+            mt={20}
+            mb={50}
+            fontSize={20}
+            bgColor={backgroundColor}
+            width="100px"
+            height="50px"
+            borderRadius="20%"
+            display="flex"
+            alignItems="center"
+            justifyContent="center">
+            <Text>{type}</Text>
+          </Box>
+
+          <Box display="flex" fontSize="15px" pt={100}>
+            <Text>Hp {hp}</Text>
+            <Text>Attack {attack}</Text>
+            <Text>Defense {defense}</Text>
+            <Text>Sp.Attack {specialAttack}</Text>
+            <Text>Sp.Defense {specialDefense}</Text>
+            <Text>Speed {speed}</Text>
+          </Box>
+        </Box>
         <Box>
           <Box display="flex" flexDir="column" alignItems="center">
             <Text as="h4" mb="5px" fontSize="30px" color="##202020">
@@ -46,7 +84,6 @@ export const PokemonStats = ({
             alignItems="center"
             justifyItems="center"
             borderRadius="15px"
-            margin="auto"
             gridTemplateColumns="repeat(auto-fit,minmax(200px,1fr))"
             fontSize="20px">
             <Box>
@@ -73,42 +110,14 @@ export const PokemonStats = ({
                 {ability}
               </Text>
             </Box>
-          </Box>
-          <Box
-            width="200px"
-            height="200px"
-            display="flex"
-            ml={40}
-            textTransform="capitalize"
-            flexDir="column"
-            alignItems="center">
-            <Image pl={40} pt={20} width="400px" height="200px" src={image} alt={name} />
-            <Box
-              textTransform="capitalize"
-              border="none"
-              mt={20}
-              mb={50}
-              fontSize={20}
-              bgColor={backgroundColor}
-              width="100px"
-              height="50px"
-              borderRadius="20%"
-              display="flex"
-              alignItems="center"
-              justifyContent="center">
-              <Text>{type}</Text>
+            <Box>
+              <Text m="5px" color="#F3F3E9">
+                Gender
+              </Text>
+              <Text m="5px" color="#F50065">
+                {gender}
+              </Text>
             </Box>
-          </Box>
-
-          <Box display="flex" fontSize="15px" pt={100}>
-            <Text>Hp {hp}</Text>
-
-            <Text>Attack {attack}</Text>
-
-            <Text>Defense {defense}</Text>
-            <Text>Sp.Attack {specialAttack}</Text>
-            <Text>Sp.Defense {specialDefense}</Text>
-            <Text>Speed {speed}</Text>
           </Box>
         </Box>
       </Box>
